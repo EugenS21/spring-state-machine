04ae2f45d2f1d3d41e518b8f9fd938dae91ea3b5
Eugeniu Savca <savcaeugen@gmail.com>
2022-11-06 11:40:55 +0200

    Initial commit

---
+++ src/test/java/com/eugens21/springstatemachine/SpringStateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eugens21.springstatemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStateMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
